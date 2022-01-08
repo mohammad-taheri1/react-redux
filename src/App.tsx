e3800cdf23375eab1449d006192cf27338105108
MamadTaheri68 <mamad.taheri.68@gmail.com>
2022-01-08 21:34:36 +0330

    test isPalindrome input type

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { isPalindrome } from './palindrome';
 
 function App() {
+  console.log(isPalindrome('Level'));
+  console.log(isPalindrome('Anna'));
+  console.log(isPalindrome('Carlos'));
+  console.log(isPalindrome(101));
+  console.log(isPalindrome(true));
+  console.log(isPalindrome(flase));
+
+
+  
   return (
     <div className="App">
       <header className="App-header">
