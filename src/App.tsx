e3714a01faf143aa391717a482d153e1d0e835b1
MamadTaheri68 <mamad.taheri.68@gmail.com>
2022-01-08 21:57:59 +0330

    multiple declare of interface

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,7 @@ import React from 'react';
 
 const App = () => {
 
-  // type declaration
-  type TUser = {
+  interface IUser {
     username: string
     email: string
     name: string
@@ -12,68 +11,25 @@ const App = () => {
     active: boolean
   }
 
-  const user: TUser = {
-    username: 'mamad_taheri',
-    email: 'mamad@mail.com',
-    name: 'Mamadddddd',
-    // age: 32,
-    website: 'irdevprogs.ir',
+  interface IUser {
+    country: string
+  }
+
+  const user: IUser = {
+    username: 'mamad.taheri',
+    email: 'mamad@tmail.com',
+    name: 'Mamad Taheri',
+    country: 'Iran',
+    age: 32,
+    website: 'www.test.ir',
     active: true
   }
 
-    // interface declaration
-    interface IUser {
-      username: string
-      email: string
-      name: string
-      age?: number
-      website: string
-      active: boolean
-    }
-
-    // Extending an interface
-    interface IWork {
-      company: string
-      position: string
-    }
-
-    interface IPerson extends IWork {
-      name: string
-      age: number
-    }
-
-    // Extending a type
-    type TWork = {
-      company: string
-      position: string
-    }
-
-    type TPerson1 = TWork & {
-      name: string
-      age: number
-    }
-
-    // Extending an interface into a type
-    type TPerson2 = IWork & {
-      name: string
-      age: number
-    }
-
-    class Person1 implements IWork {
-      name: 'Mamad'
-      age: 32
-    }
-
-    class Person2 implements TWork {
-      name: 'Mamad'
-      age: 32
-    }
-
-
-
-
-
-  return ( <div> App.tsx </div> );
+  return (
+    <div>
+      
+    </div>
+  );
 };
 
 export default App;
