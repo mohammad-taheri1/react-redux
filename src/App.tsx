e70ad5ba88c66d5fb060dd3c5b59af6ce04d336b
MamadTaheri68 <mamad.taheri.68@gmail.com>
2022-01-10 00:42:07 +0330

    convert class to functiond

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Issues_Class from './components/Issues_Class';
+import Issues_Functional from './components/Issues_Functional';
 
 class App extends Component {
   render() {
@@ -7,7 +8,8 @@ class App extends Component {
       <div style={{padding: "1rem"}}>
         App
         <hr />
-        <Issues_Class />
+        {/* <Issues_Class /> */}
+        <Issues_Functional />
       </div>
     );
   }
