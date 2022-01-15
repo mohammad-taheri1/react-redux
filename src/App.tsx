52de69e5120248158da5a4b65586cde5fdc72fc4
MamadTaheri68 <mamad.taheri.68@gmail.com>
2022-01-15 23:26:13 +0330

    refresh project

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,11 @@
-import React, { Component } from 'react';
-import Issues_Class from './components/Issues_Class';
-import Issues_Functional from './components/Issues_Functional';
+import React from 'react';
 
-class App extends Component {
-  render() {
+const App = () => {
     return (
-      <div style={{padding: "1rem"}}>
-        App
-        <hr />
-        {/* <Issues_Class /> */}
-        <Issues_Functional />
-      </div>
+        <div>
+            
+        </div>
     );
-  }
-}
+};
 
 export default App;
