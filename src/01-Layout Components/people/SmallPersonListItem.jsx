a5c9f4d383abeb28e9ccdb15baeaf0e954cbaf66
MamadTaheri68 <mamad.taheri.68@gmail.com>
2022-02-11 23:19:01 +0330

    refresh

--- src/01-Layout Components/people/SmallPersonListItem.jsx
+++
@@ -1,8 +0,0 @@
-const SmallPersonListItem = ({ person }) => {
-    const { name, age } = person;
-    return (
-         <p>Name: {name}, Age: {age} years</p>
-    );
-};
-
-export default SmallPersonListItem;
--- src/data/urls.js
+++
@@ -1,7 +0,0 @@
- const urls = {
-     currentUser: `http://localhost:3333/current-user`,
-     users: `http://localhost:3333/users`,
-     products: `http://localhost:3333/products`,
- }
-
- export default urls;
