c367c728b6a02337331b805b46354fb51b16abf2
MamadTaheri68 <mamad.taheri.68@gmail.com>
2022-03-12 22:02:18 +0330

    reset

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,9 @@
-import SideBarMenu from "./components/SideBarMenu";
-import { SideBarMenuCard, SideBarMenuItem } from "./types/types";
-import { FcAdvertising } from "react-icons/fc";
+import React from 'react';
 
 const App = () => {
-    const items:SideBarMenuItem[] = [
-        { 
-            id: '1',
-            label: 'Salam',
-            icon: FcAdvertising,
-            url: "/",
-        }
-    ]
-
-    const card: SideBarMenuCard = {
-        id: 'card01',
-        displayName: 'Mamad Taheri',
-        title: 'Developer',
-        photoUrl: '',
-        url: '/',
-    }
     return (
         <div>
-            <SideBarMenu items={items} card={card} />
+            App
         </div>
     );
 };
--- src/util/classes.ts
+++
@@ -1,3 +0,0 @@
-export function classNames(...args: any[]): string {
-    return args.filter(Boolean).join(' ');
-}
