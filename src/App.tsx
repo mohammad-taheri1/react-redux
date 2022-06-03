5926c5fe9d04781482cfc5e0aa1e0f18051d4f73
MamadTaheri68 <mamad.taheri.68@gmail.com>
2022-06-04 01:03:03 +0430

    Product

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import Product from './components/Product';
+import Products from './components/Products';
+import { IProduct } from './interfaces/IProduct';
+
+const products: IProduct[] = [
+  { id:1, title: "chair", category: "1", image: "chair.png"},
+  { id:2, title: "book", category: "1", image: "book.png"},
+  { id:3, title: "glass", category: "1", image: "glass.png"},
+]
 
 const App = () => {
   return (
     <div>
-      App
+      <Products products={products} />
     </div>
   );
 };
---
+++ src/interfaces/IProduct.ts
@@ -0,0 +1,6 @@
+export interface IProduct {
+    id: number;
+    title: string;
+    category: string;
+    image: string;
+}
