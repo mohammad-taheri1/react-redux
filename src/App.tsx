2114eaaa2169bf0927338510bc749ce7eddf4dbb
MamadTaheri68 <mamad.taheri.68@gmail.com>
2022-06-04 00:40:15 +0430

    refresh

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,9 @@
-import { Button, makeStyles } from "@material-ui/core";
-import { Person } from "@material-ui/icons";
-
-const useStyles = makeStyles(theme=>({
-    button: {
-        color: "white",
-        backgroundColor: theme.palette.primary.main,
-    }
-}));
+import React from 'react';
 
 const App = () => {
-    const classes = useStyles();
   return (
     <div>
-      <Button
-        variant="contained"
-        size="large"
-        startIcon={<Person />}
-        className={classes.button}
-      >
-        Material UI
-      </Button>
+      App
     </div>
   );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import { mainTheme } from "./themes/mainTheme";
-import { ThemeProvider } from "@material-ui/styles";
 
 ReactDOM.render(
   <React.StrictMode>
-    <ThemeProvider theme={mainTheme}>
       <App />
-    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
